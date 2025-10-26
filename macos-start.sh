b5b2f431cce1be3d3e4e1877d31a607667502812
garrdbyrd <garrett@byrd.email>
2025-10-25 23:07:36 -0400

    Added packages and organized

--- macos-start.sh
+++ macos-start.sh
@@ -1,41 +1,49 @@
+# MacOS settings to manually set
+# Settings -> Keyboard -> repeat rate -> max
 
-
-# Settings -> Keyboard -> Keyboard Shortcuts -> Modifer Keys
-#             Control key   -> Command            
-#             Option        -> Control
-#             Command       -> fn Function
-#
-# These steps are deprecated, use karabiner-elements to rebind all keys.
-#
-#
-#
-#
-
-# Home and End keys
-# sudo mkdir ~/Library/KeyBindings
-# cd ~/Library/KeyBindings
-# cat >> DefaultKeyBinding.dict << EOF
-#     "\UF729"  = moveToBeginningOfLine:; // home
-#     "\UF72B"  = moveToEndOfLine:; // end
+# old:
+# ```
+# {
+#     "@\UF703" = ("moveWordForward:", "moveWordForward:", "moveWordBackward:");
+#     "@$\UF703" = ("moveWordForwardAndModifySelection:", "moveWordForwardAndModifySelection:", "moveWordBackwardAndModifySelection:");
+#     "\UF729" = "moveToBeginningOfLine:";
+#     "\UF72B" = "moveToEndOfLine:";
 #     "$\UF729" = moveToBeginningOfLineAndModifySelection:; // shift-home
 #     "$\UF72B" = moveToEndOfLineAndModifySelection:; // shift-end
-#     "^\UF703" = ("moveWordForward:", "moveWordForward:", "moveWordBackward:");
-#     "^$\UF703" = ("moveWordForwardAndModifySelection:", "moveWordForwardAndModifySelection:", "moveWordBackwardAndModifySelection:");
-# EOF
+#     "@\UF729" = moveToBeginningOfDocument:; // ctrl-home
+#     "@\UF72B" = moveToEndOfDocument:; // ctrl-end
+#     "@$\UF729" = moveToBeginningOfDocumentAndModifySelection:; // ctrl-shift-home
+#     "@$\UF72B" = moveToEndOfDocumentAndModifySelection:; // ctrl-shift-end
+# }
+
 # Home, End, and Ctrl+Arrow key bindings for macOS
+# (not quite right, still needs adjustment, but mostly works)
 mkdir -p ~/Library/KeyBindings
 cd ~/Library/KeyBindings
 cat > DefaultKeyBinding.dict << 'EOF'
 {
-    "\UF729"  = moveToBeginningOfLine;                     // Home
-    "\UF72B"  = moveToEndOfLine;                           // End
-    "$\UF729" = moveToBeginningOfLineAndModifySelection;   // Shift+Home
-    "$\UF72B" = moveToEndOfLineAndModifySelection;         // Shift+End
+    // Ctrl (remapped Command) + Right / Left → move by word
+    "@\UF703"  = ("moveWordForward:", "moveWordForward:", "moveWordBackward:");
+    "@\UF702"  = ("moveWordBackward:", "moveWordBackward:", "moveWordForward:");
 
-    "^\UF702"  = moveWordBackward;                         // Ctrl+Left
-    "^\UF703"  = moveWordForward;                          // Ctrl+Right
-    "^$\UF702" = moveWordBackwardAndModifySelection;       // Ctrl+Shift+Left
-    "^$\UF703" = moveWordForwardAndModifySelection;        // Ctrl+Shift+Right
+    // Ctrl (remapped Command) + Shift + Right / Left → select by word
+    "@$\UF703" = ("moveWordForwardAndModifySelection:", "moveWordForwardAndModifySelection:", "moveWordBackwardAndModifySelection:");
+    "@$\UF702" = ("moveWordBackwardAndModifySelection:", "moveWordBackwardAndModifySelection:", "moveWordForwardAndModifySelection:");
+
+    // Home / End → line navigation
+    "\UF729"   = moveToBeginningOfLine:;
+    "\UF72B"   = moveToEndOfLine:;
+    "$\UF729"  = moveToBeginningOfLineAndModifySelection:;
+    "$\UF72B"  = moveToEndOfLineAndModifySelection:;
+
+    // Ctrl (remapped Command) + Home / End → document navigation
+    "@\UF729"  = moveToBeginningOfDocument:;
+    "@\UF72B"  = moveToEndOfDocument:;
+    "@$\UF729" = moveToBeginningOfDocumentAndModifySelection:;
+    "@$\UF72B" = moveToEndOfDocumentAndModifySelection:;
+
+    // Alt+Backspace → delete previous word
+    "~\U0008"  = deleteWordBackward:;
 }
 EOF
 
@@ -49,28 +57,44 @@ EOF
 # homebrew
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip-components 1 -C homebrew
 
-# Rectangle
+# 
+# User interface / usability
+#
+# Rectangle (control windows)
 brew install --cask rectangle
-# Swift Quit
+#   manually import config
+# Swift Quit (make quitting programs actually make them stop)
 brew install --cask swift-quit
-# Alt Tab
+#   all apps except the following: Logic Pro X
+# Alt Tab 
 brew install --cask alt-tab
-# FireFox
-brew install --cask firefox
-# Sound Control ?
-brew install --cask sound-control
-# krita
-brew install --cask krita
-# karabiner-elements
+# karabiner-elements (bind Cmd -> Ctrl, etc)
 brew install --cask karabiner-elements
-# mos ?
+cp configs/karabiner.json ~/.configs/karabiner/karabiner.json
+# mos (smooth scrolling)
 brew install mos
-# libre office
-brew install --cask libreoffice
-# betterdisplay
-brew install --cask betterdisplay
-# mute individual apps
+# Background Music (mute individual apps)
 brew install --cask background-music
 
-# disable scroll acceleration
-defaults write -g com.apple.scrollwheel.scaling -1
+#
+# Applications 
+#
+# libre office
+brew install --cask libreoffice
+# krita
+brew install --cask krita
+# betterdisplay
+brew install --cask betterdisplay
+# linear mouse
+brew install --cask linearmouse
+# FireFox
+brew install --cask firefox
+
+# others to consider:
+# Raycast
+# brew install --cask raycast
+# Sound Control (use Background Music instead)
+#brew install --cask sound-control
+
+# disable scroll acceleration (possibly not necessary since linear mouse install)
+# defaults write -g com.apple.scrollwheel.scaling -1
